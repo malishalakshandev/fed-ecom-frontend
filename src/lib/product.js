f5b2f7e23a069380becae9cf71d8711ddaa09797
Malisha Rosa <malishalakshandev@gmail.com>
2025-08-08 20:32:53 +0530

    add production base url to image upload

--- src/lib/product.js
+++ src/lib/product.js
@@ -1,5 +1,5 @@
 export const putImage = async ({ file }) => {
-  const res = await fetch(`http://localhost:8000/api/products/images`, {
+  const res = await fetch(`https://fed-2-back-end-malisha.vercel.app/api/products/images`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
